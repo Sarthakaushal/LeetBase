5534bf3f50d1b6e05f96b73aff554ef8474248f4
Sarthakaushal <sarthakkaushal@outlook.com>
2024-01-05 01:24:47 -0500

    day 1

---
+++ Arrays_n_Hashes/P1-contains-duplicate.py
@@ -0,0 +1,26 @@
+# Link to Ques : https://leetcode.com/problems/contains-duplicate/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        """
+        Approach 1 brute force
+        => Check difference in the length of the list and the set
+        => Time complexity  : O(n)
+        => Space complexity : O(n)
+        """
+        if len(nums)!=len(list(set(nums))):
+            return True
+        else:
+            return False
+        """
+        Approach 2 brute force
+        => sort list then check for consecutive elements
+        => Time complexity  : O(n*logn)
+        => Space complexity : O(n)
+        """
+        
+        # nums.sort()
+        # for i in range(1,len(nums)):
+        #     if nums[i-1] == nums[i]:
+        #         return True
+        # return False
