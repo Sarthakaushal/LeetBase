43edbed6e8d6de961bfbab2d2112639d4250fbdf
Sarthakaushal <sarthakkaushal@outlook.com>
2024-01-07 23:03:05 -0500

    Accepted solution for the two sum problem

--- Arrays_n_Hashes/P2.py
+++ Arrays_n_Hashes/P2.py
@@ -1,33 +1,18 @@
-# Link to Ques : https://leetcode.com/problems/two-sum
 from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
-        Brute Force - sort the list and traverse
-        => Time comolexity : O(n*logn)
-        => Space complexity: ??
+        Adapted from : 3 Method's || C++ || JAVA || PYTHON || Beginner Friendly🔥🔥🔥
+        => Time comolexity : O(n)
+        => Space complexity: O(n)
         """
-        f = sorted(nums)
-        i =0
-        j=1
-        is_i = False
-        while f[i]+f[j] != target:
-            if f[i]+f[j] < target:
-                if j < len(f)-1:
-                    j+=1
-                elif i < j-1:
-                    i+=1
+        traversed_elements = {}
+        for ind in range(len(nums)):
+            ele = nums[ind]
+            component = target - ele
+            if component in traversed_elements.keys():
+                return [ind, traversed_elements[component]]
             else:
-                if not is_i:
-                    i+=1
-                    is_i = not is_i
-                else:
-                    j+=1
-                    is_i = not is_i
-                # elif j-i > 1:
-                #     j-=1
-        try:
-            return [nums.index(f[i]),i+1+nums[i+1:].index(f[j])]
-        except:
-            return [nums.index(f[i]), nums.index(f[j])]
+                traversed_elements[ele] = ind
+        return [None]
